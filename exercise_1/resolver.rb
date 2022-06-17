470aa735415bf08fb815169a499565c6ec96e9c5
tim-luong-goldenowl <tim.luong.goldenowl@gmail.com>
2022-06-17 16:53:47 +0700

    Finished exercise 1

--- exercise_1/resolver.rb
+++ exercise_1/resolver.rb
@@ -1,1 +1,78 @@
-# write code here...
+def find_desired_pairs (input)
+    number_list = input.split(',').map(&:to_i)
+    
+    desired_sum = number_list[0]
+    input_array = number_list.drop(1)
+    
+    needed_to_fullfill_array = input_array.map do |e|
+        desired_sum - e
+    end
+  
+    input_array_as_hash = {}
+
+    input_array.each do |e|
+        input_array_as_hash[e] = e
+    end
+
+    result = []
+
+    index = 0
+
+    input_array.each do |e|
+        need_to_fullfill_number = needed_to_fullfill_array[index]
+
+        fullfill_number_for_this_element = input_array_as_hash[need_to_fullfill_number]
+        next unless input_array_as_hash[need_to_fullfill_number]
+
+        if e + input_array_as_hash[need_to_fullfill_number] == desired_sum
+            result << [e, input_array_as_hash[need_to_fullfill_number]]
+            input_array_as_hash[need_to_fullfill_number] = -1
+        end
+
+        index += 1
+    end
+
+    result
+  end
+
+class Resolver
+    def find_desired_pairs (input)
+        number_list = input.split(',').map(&:to_i)
+        
+        desired_sum = number_list[0]
+        input_array = number_list.drop(1)
+        
+        needed_to_fullfill_array = input_array.map do |e|
+            desired_sum - e
+        end
+      
+        input_array_as_hash = {}
+    
+        input_array.each do |e|
+            input_array_as_hash[e] = e
+        end
+    
+        result = []
+    
+        index = 0
+    
+        input_array.each do |e|
+            need_to_fullfill_number = needed_to_fullfill_array[index]
+    
+            fullfill_number_for_this_element = input_array_as_hash[need_to_fullfill_number]
+            next unless input_array_as_hash[need_to_fullfill_number]
+    
+            if e + input_array_as_hash[need_to_fullfill_number] == desired_sum
+                result << [e, input_array_as_hash[need_to_fullfill_number]]
+                input_array_as_hash[need_to_fullfill_number] = -1
+            end
+    
+            index += 1
+        end
+    
+        result
+    end
+end
+  
+  
+  find_desired_pairs('8,5,3,4,5')
--- exercise_1/resolver_test.rb
+++ exercise_1/resolver_test.rb
@@ -1,1 +1,32 @@
-# write code here...
+require_relative './resolver'
+
+RSpec.describe "find_desired_pairs method", :type => :request do
+    let(:input_1) { ('8,5,3,4,5') }
+    let(:input_2) { ('9,5,3,4,5') }
+    describe "with input 1" do
+        it "check find_desired_pairs to return correct data length (3) with input 1" do
+            result = Resolver.send(:find_desired_pairs, input_1)
+            expect(result.length).to   eq(3)
+        end
+
+        it "check find_desired_pairs to return correct data value" do
+            result = Resolver.send(:find_desired_pairs, input_1)
+            expect(result[0]).to   eq([5,3])
+            expect(result[1]).to   eq([3,5])
+            expect(result[2]).to   eq([4,4])
+        end
+    end
+    
+    describe "with input 2" do
+        it "check find_desired_pairs to return correct data length (1) with input 1" do
+            result = Resolver.send(:find_desired_pairs, input_2)
+            expect(result.length).to   eq(1)
+        end
+
+        it "check find_desired_pairs to return correct data value" do
+            result = Resolver.send(:find_desired_pairs, input_2)
+            expect(result[0]).to   eq([5,4])
+        end
+    end
+  end
+  
